d59b1f7330ea325efd28a2c2fee3e4ad7a0f3a9f
Belkees-alsayasinah <sayasinah2017@gmail.com>
2023-01-09 17:17:22 +0200

    test1

--- main/Main.java
+++ main/Main.java
@@ -3,12 +3,8 @@ package main;
 public class Main {
     public static void main(String []args){
         System.out.println("test");
-
-        System.out.println("test1");
-        System.out.println("test2");
-
-        //ddfdf
-        System.out.println("test2");
-
     }
 }
+//git add .
+//git commit -m "name"
+//git push origin main
